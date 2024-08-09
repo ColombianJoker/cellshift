93c13ca4bd208d48e554ec5dc826e4683b1c99e1
ColombianJoker <elbarrios1@poligran.edu.co>
2024-08-09 11:06:45 -0500

    First functionality

--- __init__.py
+++ __init__.py
@@ -2,3 +2,22 @@
 # Tool to transform, obfuscate and anonymize datasets
 # Ramón Barrios Láscar
 # 2024-08-09
+
+import duckdb as d
+import pandas as pd
+import polars as pl
+
+if 'duckdb_verbose' in globals():
+  import sys
+
+if 'duckdb_config' in globals():
+  cx=d.connect(
+    config=duckdb_config
+  )
+  if ('duckdb_verbose' in globals()) and duckdb_verbose:
+    print(f"DuckDB connected to default", file=sys.stderr)
+else:
+  cx=d.connect()
+  if ('duckdb_verbose' in globals()) and duckdb_verbose:
+    print(f"DuckDB connected to default", file=sys.stderr)
+  
