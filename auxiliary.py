e333e49f7f8faf52c5b83b4a13e7477616df227e
ColombianJoker <elbarrios1@poligran.edu.co>
2024-08-13 19:39:24 -0500

    Includes add_columns()

--- auxiliary.py
+++ auxiliary.py
@@ -1,6 +1,8 @@
 import os
 import re
 import duckdb as d
+import pandas as pd
+import numpy as np
 from typing import Generator
 from . import cx, tn, tn_prefix, tn_sep
 
@@ -11,28 +13,11 @@ def table_name(name_list):
     parts=name_list[-1].split(tn_sep)
     last_one=parts[-1]
 
-def to_table(object):
-  """
-  Convert given object to internal DuckDB relation representation
-  """
-    
-  if isinstance(object,d.duckdb.DuckDBPyRelation):
-    try:
-      object_name=object.__name__
-    except AttributeError as e:
-      object_name=generate_name()
-
-    cx.register(f"{object_name}_tmp",object)
-    cx.sql(f"CREATE OR REPLACE TABLE {object_name} AS SELECT * FROM {object_name}_tmp")
-    if ('duckdb_verbose' in globals()) and duckdb_verbose:
-      print(f"{object_name=}")
-    return cx.sql(f"SELECT * FROM {object_name}")
-  else:
-    return object
-
 def set_type(object,column,new_type):
-  if isinstance(object, dict) and ('n' in object):
-    d.execute(f"ALTER TABLE {object['n']} ALTER COLUMN {column} TYPE {new_type}")
+  if isinstance(object, d.duckdb.DuckDBPyRelation):
+    # Return everything except the named column then the casted named column
+    new_rel=object.project(f"* EXCLUDE {column}, CAST({column} AS {new_type}) AS {column}")
+    return new_rel
 
 def name_generator()->Generator[str, None, None]:
   """
@@ -43,4 +28,24 @@ def name_generator()->Generator[str, None, None]:
     yield f"{tn_prefix}{tn_sep}{seq}"
     seq+=1
 
+def add_columns(relobject, colobject, colname=None):
+  if isinstance(relobject, d.duckdb.DuckDBPyRelation) and \
+    isinstance(colobject, pd.core.frame.DataFrame):
+    # Generate new names for objects as tables
+    new_rel=next(tn_gen)
+    new_pd=next(tn_gen)
+    # Register objects as tables
+    d.register(new_rel, relobject)
+    d.register(new_pd, colobject)
+    if colname:
+      return d.sql(f"SELECT {new_rel}.*, {new_pd}.* AS {colname} FROM {new_rel} POSITIONAL JOIN {new_pd}")
+    else:
+      return d.sql(f"SELECT {new_rel}.*, {new_pd}.* FROM {new_rel} POSITIONAL JOIN {new_pd}")
+  if isinstance(relobject, d.duckdb.DuckDBPyRelation) and \
+    isinstance(colobject, np.ndarray):
+    if colobject.ndim<2 or (colobject.ndim==2 and ((colobject.shape[0]==1) or (colobject.shape[1]==1))):
+      return d.sql(f"SELECT relobject.*, colobject.* AS {colname} FROM relobject POSITIONAL JOIN colobject")
+    else:
+      return d.sql(f"SELECT relobject.*, colobject.* FROM relobject POSITIONAL JOIN colobject")
+    
 tn_gen=name_generator()
